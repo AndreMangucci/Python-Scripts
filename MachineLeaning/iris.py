8f624d742f62c3db783cd7b54933f5cc4bc8d3b6
AndreMangucci <andremangucci@gmail.com>
2019-08-11 22:13:37 -0300

    Estudos em machine learning

--- MachineLeaning/iris.py
+++ MachineLeaning/iris.py
@@ -12,19 +12,44 @@ test_idx = [0,50,100] # there is 50 example of witch label respectively starting
 #labels = iris.target[0]
 #features = iris.data[0]
 
-#traing data
+#traing data - uses numpy to delete the train data 
 train_target = np.delete(iris.target,test_idx) 
 train_data = np.delete(iris.data,test_idx,axis=0)
 
-#test data 
+# print("Train data")
+# print(train_target)
+# print(train_data)
+
+#test data only gets the data that was removed from the data set
 test_target = iris.target[test_idx]
 test_data = iris.data[test_idx]
 
+# print("Test data")
+# print(test_target)
+# print(test_data)
+
 clf = tree.DecisionTreeClassifier()
 clf.fit(train_data, train_target)
-
 
 expected_answer = test_target
 answer = clf.predict(test_data)
 print (expected_answer)
 print (answer)
+
+#Decision tree
+from sklearn.externals.six import StringIO
+import pydotplus as pydot
+dot_data = StringIO()
+tree.export_graphviz(
+    clf,
+    out_file=dot_data,
+    feature_names=iris.feature_names,
+    class_names=iris.target_names,
+    filled=True,
+    rounded=True,
+    impurity=False
+)
+
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+#graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
--- MachineLeaning/machineLearning.py
+++ MachineLeaning/machineLearning.py
@@ -1,7 +1,7 @@
 from sklearn import tree
-features = [[125,1],[140,1],[140,0],[150,0]]
-labels = [1,1,0,0]
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features,labels)
+features = [[125,1],[140,1],[140,0],[150,0]] #characteristics, in this case fruit weight followed by texture, 1 for smooth and 0 for bumpy
+labels = [1,1,0,0] #data label 1 for apple and 0 for orange 
+clf = tree.DecisionTreeClassifier() #it create a empty decision tree
+clf = clf.fit(features,labels) #find patterns in the provided features to create rules for the decision tree
 answer = clf.predict([[180,1]])
 print(answer)
